4e8276a7dfcef791fb50d6a0a43c67c503a97aa0
Berkeley True <berkeleytrue@users.noreply.github.com>
2024-08-25 21:58:10 -0700

    wip: won't build with slint deps

--- build.rs
+++ build.rs
@@ -1,3 +1,10 @@
 fn main() {
+    slint_build::compile_with_config(
+        "ui/appwindow.slint",
+        slint_build::CompilerConfiguration::new()
+            .embed_resources(slint_build::EmbedResourcesKind::EmbedForSoftwareRenderer),
+    )
+    .unwrap();
+    slint_build::print_rustc_flags().unwrap();
     println!("cargo:rustc-link-arg-bins=-Tlinkall.x");
 }
